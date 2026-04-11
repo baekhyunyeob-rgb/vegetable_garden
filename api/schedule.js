4651556a27f6e32a8e923eb9a5ff550d3aaa5f44
baekhyunyeob-rgb <baekhyunyeob@gmail.com>
2026-04-11 16:36:26 +0900

    500 오류 수정

--- api/schedule.js
+++ api/schedule.js
@@ -1,37 +1,35 @@
-import https from 'https';
+const https = require('https');
 
-export default async function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   
   const cntntsNo = req.query && req.query.cntntsNo;
   if (!cntntsNo) return res.status(400).json({ error: 'cntntsNo required' });
 
   const KEY = '20260409M8NZ3DE2W1X8T00CUUUHCA';
-  const path = '/service/farmWorkingPlanNew/workScheduleEraInfoJsonLst?apiKey=' + KEY + '&cntntsNo=' + cntntsNo;
+  const path = '/service/farmWorkingPlanNew/workScheduleEraInfoJsonLst?apiKey=' + KEY + '&cntntsNo=' + encodeURIComponent(cntntsNo);
 
-  return new Promise((resolve) => {
-    const options = {
-      hostname: 'api.nongsaro.go.kr',
-      port: 443,
-      path: path,
-      method: 'GET',
-    };
+  const options = {
+    hostname: 'api.nongsaro.go.kr',
+    port: 80,
+    path: path,
+    method: 'GET',
+  };
 
-    const request = https.request(options, (response) => {
-      let data = '';
-      response.on('data', (chunk) => { data += chunk; });
-      response.on('end', () => {
-        res.setHeader('Content-Type', 'application/xml; charset=utf-8');
-        res.status(200).send(data);
-        resolve();
-      });
+  // http로 시도 (농사로는 80포트)
+  const http = require('http');
+  const request = http.request(options, function(response) {
+    let data = '';
+    response.on('data', function(chunk) { data += chunk; });
+    response.on('end', function() {
+      res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+      res.status(200).send(data);
     });
-
-    request.on('error', (e) => {
-      res.status(500).json({ error: e.message });
-      resolve();
-    });
-
-    request.end();
   });
-}
+
+  request.on('error', function(e) {
+    res.status(500).json({ error: e.message, code: e.code });
+  });
+
+  request.end();
+};
